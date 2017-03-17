40efe341ca79e8b878b673853215c8d8e05dfea6
MarisaTubaugh <tubaughm1@hawkmail.newpaltz.edu>
2017-03-17 19:39:12 -0400

    Add files via upload

--- Brick.cs
+++ Brick.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-	int hits = 3;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-
-	void OnCollisionExit2D(Collision2D coll)
-	{
-		hits = hits - 1;
-		if (hits == 0){
-			Break();
-		}
-	}
-	void Break(){
-        gameObject.SetActive (false);
+    
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        gameObject.SetActive(false);
     }
-	}
+
+    
+    
+}
